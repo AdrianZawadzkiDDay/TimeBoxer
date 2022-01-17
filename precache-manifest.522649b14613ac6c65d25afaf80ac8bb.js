abce3bcef9f75c4c443658a17546c9cd2fad5961
Adrian Zawadzki <zawadzkii@tlen.pl>
2022-01-17 19:11:32 +0100

    Updates

---
+++ precache-manifest.522649b14613ac6c65d25afaf80ac8bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38f93e0334edcbcd2e3508f403e4d51",
+    "url": "/TimeBoxer/index.html"
+  },
+  {
+    "revision": "f99e6895a3ae8fc86741",
+    "url": "/TimeBoxer/static/css/main.3acbf34a.chunk.css"
+  },
+  {
+    "revision": "6e2b8ff788672eecd61d",
+    "url": "/TimeBoxer/static/js/2.aefd7cd1.chunk.js"
+  },
+  {
+    "revision": "8ddbe4d64be5a13b1134",
+    "url": "/TimeBoxer/static/js/3.6f4a24c1.chunk.js"
+  },
+  {
+    "revision": "b165a7f9de5f6f5e9fd2",
+    "url": "/TimeBoxer/static/js/4.535e5f42.chunk.js"
+  },
+  {
+    "revision": "fcaa335ec74d7915886a",
+    "url": "/TimeBoxer/static/js/5.5bacba8d.chunk.js"
+  },
+  {
+    "revision": "f99e6895a3ae8fc86741",
+    "url": "/TimeBoxer/static/js/main.40859cf6.chunk.js"
+  },
+  {
+    "revision": "2db6b67701b18b7cd843",
+    "url": "/TimeBoxer/static/js/runtime~main.1903fd70.js"
+  }
+]);
